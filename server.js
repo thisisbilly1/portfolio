9a3f35d333b3690ed3b7ff93e0dc8b019ea07408
Nick Koerber <thisisbilly1@gmail.com>
2022-11-05 14:26:21 -0500

    got rid of back end

--- server.js
+++
@@ -1,7 +0,0 @@
-import createApp from "./app.js"
-
-const app = createApp()
-
-//start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}.`) })
