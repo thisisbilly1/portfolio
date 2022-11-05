f67bbeda3460ab28275fb5d0cd5c11183e902802
Nick Koerber <thisisbilly1@gmail.com>
2022-11-05 15:05:53 -0500

    fixed routing

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/main.scss";
 // import "./assets/Themes.scss";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
 
